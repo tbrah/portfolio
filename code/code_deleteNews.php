816222928592b28ef23fc91f870d008fbd7113c9
tbrah <thomasmessell@gmail.com>
2017-07-05 22:08:28 +0200

    delete now also deletes in showcase_tech

--- code/code_deleteNews.php
+++ code/code_deleteNews.php
@@ -13,6 +13,18 @@
 
 	$crud->SendSQL();
 
+	//Delete in showcase_tech aswell
+
+	$delete_showcase_id = new Crud($objCon, "showcase_tech");
+
+	$showcase_tech_array = array("showcase_id"=>"$id");
+
+	$delete_showcase_id->SetArray($showcase_tech_array);
+
+	$delete_showcase_ids = $delete_showcase_id->Delete();
+
+	$delete_showcase_id->SendSQL();
+
 
 	header('Location: ../admin/index.php?delete=1');
 
